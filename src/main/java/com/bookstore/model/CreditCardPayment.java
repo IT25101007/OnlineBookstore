fcf4a2109ec9adbadf00d50c6fa4f77088fc2908
Perera S.A.S.S <it25102972@my.sliit.lk>
2026-05-05 12:35:52 +0530

    Add Payment Module - Payment Management Module

---
+++ src/main/java/com/bookstore/model/CreditCardPayment.java
@@ -0,0 +1,38 @@
+package com.bookstore.model;
+
+public class CreditCardPayment extends Payment {
+    private String cardLastFour;
+
+    public CreditCardPayment() {
+        setMethod("CREDIT_CARD");
+    }
+
+    public CreditCardPayment(String paymentID, String orderID, double amount, String date, String status, String cardLastFour) {
+        super(paymentID, orderID, amount, "CREDIT_CARD", date, status);
+        this.cardLastFour = cardLastFour;
+    }
+
+    public String getCardLastFour() {
+        return cardLastFour;
+    }
+
+    public void setCardLastFour(String cardLastFour) {
+        this.cardLastFour = cardLastFour;
+    }
+
+    @Override
+    public String processPayment() {
+        return "Processing credit card ending in " + cardLastFour;
+    }
+
+    @Override
+    public String toString() {
+        return "CreditCardPayment{" +
+                "paymentID='" + getPaymentID() + '\'' +
+                ", orderID='" + getOrderID() + '\'' +
+                ", amount=" + getAmount() +
+                ", cardLastFour='" + cardLastFour + '\'' +
+                ", status='" + getStatus() + '\'' +
+                '}';
+    }
+}

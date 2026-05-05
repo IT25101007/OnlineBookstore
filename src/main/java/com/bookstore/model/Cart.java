36b9ecc5d8c4f5b8b00cdc89929a2b5b8919ffd0
IT25102226 <it25102226@my.sliit.lk>
2026-05-05 12:20:33 +0530

    Add Cart Module - Add getters and setters to Cart.java

--- src/main/java/com/bookstore/model/Cart.java
+++ src/main/java/com/bookstore/model/Cart.java
@@ -17,4 +17,36 @@ public class Cart {
         this.quantity = quantity;
     }
 
+    public String getCartID() {
+        return cartID;
+    }
+
+    public void setCartID(String cartID) {
+        this.cartID = cartID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(String bookID) {
+        this.bookID = bookID;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
 }

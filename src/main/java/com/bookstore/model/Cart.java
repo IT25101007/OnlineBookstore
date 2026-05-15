7e9d2eb9a991c0a938b291abd7ef44ca2eb0136c
Perera S.A.S.S <it25102972@my.sliit.lk>
2026-05-15 22:18:18 +0530

    Component 03: Order Management & Shopping Cart — Perera S.A.S.S adding Component 03

---
+++ src/main/java/com/bookstore/model/Cart.java
@@ -0,0 +1,80 @@
+package com.bookstore.model;
+
+public class Cart {
+
+    private String cartID;
+    private String userID;
+    private String bookID;
+    private int quantity;
+
+
+    public Cart() {
+    }
+
+
+    public Cart(String cartID, String userID, String bookID, int quantity) {
+        this.cartID = cartID;
+        this.userID = userID;
+        this.bookID = bookID;
+        this.quantity = quantity;
+    }
+
+
+    public String getCartID() {
+        return cartID;
+    }
+
+    public void setCartID(String cartID) {
+        this.cartID = cartID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(String bookID) {
+        this.bookID = bookID;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+
+
+    public String toFileString() {
+        return cartID + " | " + userID + " | " + bookID + " | " + quantity;
+    }
+
+
+    public static Cart fromFileString(String line) {
+        String[] parts = line.split(" \\| ");
+        if (parts.length == 4) {
+            return new Cart(parts[0], parts[1], parts[2], Integer.parseInt(parts[3]));
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "cartID='" + cartID + '\'' +
+                ", userID='" + userID + '\'' +
+                ", bookID='" + bookID + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
+// updated

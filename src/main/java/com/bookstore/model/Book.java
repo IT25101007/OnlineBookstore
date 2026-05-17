06c4da40097323670c956dafe8fdcc32fe8d8fb3
IT25102674 <it25102674@my.sliit.lk>
2026-05-17 10:47:19 +0530

    Book.java class completed

--- src/main/java/com/bookstore/model/Book.java
+++ src/main/java/com/bookstore/model/Book.java
@@ -82,4 +82,39 @@ public abstract class Book {
         this.type = type;
     }
 
+    // Abstract method - polymorphism
+    public abstract String displayFormat();
+
+    // Concrete method to convert to file string
+    public String toFileString() {
+        return bookID + " | " + title + " | " + author + " | " + category + " | " + price + " | " + stock + " | " + type;
+    }
+
+    // Static factory method to parse and create appropriate subclass
+    public static Book fromFileString(String line) {
+        String[] parts = line.split(" \\| ");
+        if (parts.length == 7) {
+            String type = parts[6];
+            if ("EBOOK".equalsIgnoreCase(type)) {
+                return new EBook(parts[0], parts[1], parts[2], parts[3],
+                        Double.parseDouble(parts[4]), Integer.parseInt(parts[5]), "");
+            } else {
+                return new PrintedBook(parts[0], parts[1], parts[2], parts[3],
+                        Double.parseDouble(parts[4]), Integer.parseInt(parts[5]), 0);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookID='" + bookID + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                ", stock=" + stock +
+                ", type='" + type + '\'' +
+                '}';
+    }
 }
